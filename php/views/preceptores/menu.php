96ff3399263b6ba13a053307f9b22edfe86a7e03
JhomarMendieta <dkname02@gmail.com>
2024-05-21 19:05:21 -0300

    preceptores

--- php/views/preceptores/menu.php
+++ php/views/preceptores/menu.php
@@ -1,20 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/navStyle.css">
 </head>
+
 <body>
     <header class="header">
         <nav class="navbar" id="navlateral">
             <div class="logo">
-                <img src="../../../img/LogoEESTN1.png" alt="" class="logoEEST1">
+                <img src="../../../img/LogoEESTN1.png" alt="Logo de la EEST 1">
+                <label for="navlateral">
+                    <div class="lienas">
+                        <div class="linea l1"></div>
+                        <div class="linea l2"></div>
+                        <div class="linea l3"></div>
+                    </div>
+                    
+                </label>
+            </div>
+            <h2>INICIO</h2>
+            <div class="usuario">
+                <p class="tipo_usuario">Preceptor</p>
+                <div class="icon">
+                    <i class="fa-solid fa-user"></i>
+                </div>
             </div>
         </nav>
         <nav class="navlateral" id="navlateral">
 
         </nav>
     </header>
+
+
+
+    <script src="https://kit.fontawesome.com/d5f1649c82.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
