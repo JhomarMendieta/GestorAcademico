38655bafad61862a1675d772956ab1aeafd7e6a8
JhomarMendieta <dkname02@gmail.com>
2024-05-21 18:32:18 -0300

    hola

---
+++ php/views/preceptores/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="navbar" id="navlateral">
+            <div class="logo">
+                <img src="../../../img/LogoEESTN1.png" alt="" class="logoEEST1">
+            </div>
+        </nav>
+        <nav class="navlateral" id="navlateral">
+
+        </nav>
+    </header>
+</body>
+</html>
