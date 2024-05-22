5243a85388607f212e5e5635bd52d3fd946c2b48
JhomarMendieta <dkname02@gmail.com>
2024-05-21 21:17:20 -0300

    preceptores - Jhomar

--- php/views/preceptores/menu.php
+++ php/views/preceptores/menu.php
@@ -35,10 +35,66 @@
         <nav class="navlateral">
             <div class="logo">
                 <img src="../../../img/LogoEESTN1.png" alt="">
+                <label for="navlateral" class="cruz">
+                    <div class="linea l1"></div>
+                    <div class="linea l2"></div>
+                </label>
             </div>
         </nav>
     </header>
-
+    <p>
+        Tu número en un papel <br>
+        Recuerdos de una canción <br>
+        Tardes pretendiendo ver televisión <br>
+        Primero siente la piel <br>
+        Y luego lo siente el corazón <br>
+        Por ti iré a tu calle, me hace ver que <br>
+        Recuerdo por qué me enamoré <br>
+        Recuerdo lo que te enamoró <br>
+        Pero estoy llamando porque muero por saber<br>
+        ¿Cómo estás?<br>
+        No sé por qué pero pensé en los dos<br>
+        Moría de ganas de escuchar tu voz<br>
+        Se siente igual, nada cambió<br>
+        ¿Cómo estás?<br>
+        Sigues viviendo en el 506<br>
+        Donde dejamos el primer amor<br>
+        Con 16, todo cambió<br>
+        Recuerdo por qué me enamoré<br>
+        Recuerdo lo que te enamoró<br>
+        Pero contestaste y ya se me olvidó por qué se acabó<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh<br>
+        Se acabó<br>
+        Ayer jugamos a escondidas con otras intenciones<br>
+        Hoy te encuentro escondida en más de 15 canciones<br>
+        Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
+        Es apenas normal que me dan ganas de saber<br>
+        Si sigues yendo a Cartagena de vacaciones<br>
+        Si todavía le tienes miedo a los aviones<br>
+        Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
+        Es apenas normal que me dan ganas de saber<br>
+        ¿Cómo estás?<br>
+        No sé por qué pero pensé en los dos<br>
+        Yo moría de ganas de escuchar tu voz<br>
+        Se siente igual, nada cambió<br>
+        ¿Cómo estás?<br>
+        Sigues viviendo en el 506<br>
+        Donde dejamos el primer amor<br>
+        Con 16, todo cambió<br>
+        Recuerdo por qué me enamoré<br>
+        Recuerdo lo que te enamoró<br>
+        Pero contestaste y ya se me olvidó por qué se acabó<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh<br>
+        Se acabó<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+        Oh-oh oh-oh, oh-oh oh-oh<br>
+        Se acabó
+    </p>
 
 
     <script src="https://kit.fontawesome.com/d5f1649c82.js" crossorigin="anonymous"></script>
