03924652798699e61adf0e992924c9a71dff3bce
JhomarMendieta <dkname02@gmail.com>
2024-05-28 21:03:05 -0300

    jhomar - perfil preceptores

--- php/views/preceptores/menu2.php
+++ php/views/preceptores/menu2.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../../img/LogoEESTN1.png" type="image/x-icon">
     <title>Document</title>
     <link rel="stylesheet" href="css/navStyle.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
@@ -20,7 +22,7 @@
                         <div class="linea l2"></div>
                         <div class="linea l3"></div>
                     </div>
-                    
+
                 </label>
             </div>
             <h2>INICIO</h2>
@@ -41,74 +43,113 @@
                 </label>
             </div>
             <div class="opciones">
-            <p class="principal">Alumnos</p>
-            <p></p>
-            <p></p>
-            <p class="principal">Profesores</p>
-            <p></p>
-            <p></p>
-            <p class="principal">Materias</p>
-            <p></p>
-            <p></p>
+                <p class="principal">Alumnos</p>
+                <p></p>
+                <p></p>
+                <p class="principal">Profesores</p>
+                <p></p>
+                <p></p>
+                <p class="principal">Materias</p>
+                <p></p>
+                <p></p>
             </div>
         </nav>
     </header>
-    <p>
-        Tu número en un papel <br>
-        Recuerdos de una canción <br>
-        Tardes pretendiendo ver televisión <br>
-        Primero siente la piel <br>
-        Y luego lo siente el corazón <br>
-        Por ti iré a tu calle, me hace ver que <br>
-        Recuerdo por qué me enamoré <br>
-        Recuerdo lo que te enamoró <br>
-        Pero estoy llamando porque muero por saber<br>
-        ¿Cómo estás?<br>
-        No sé por qué pero pensé en los dos<br>
-        Moría de ganas de escuchar tu voz<br>
-        Se siente igual, nada cambió<br>
-        ¿Cómo estás?<br>
-        Sigues viviendo en el 506<br>
-        Donde dejamos el primer amor<br>
-        Con 16, todo cambió<br>
-        Recuerdo por qué me enamoré<br>
-        Recuerdo lo que te enamoró<br>
-        Pero contestaste y ya se me olvidó por qué se acabó<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh<br>
-        Se acabó<br>
-        Ayer jugamos a escondidas con otras intenciones<br>
-        Hoy te encuentro escondida en más de 15 canciones<br>
-        Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
-        Es apenas normal que me dan ganas de saber<br>
-        Si sigues yendo a Cartagena de vacaciones<br>
-        Si todavía le tienes miedo a los aviones<br>
-        Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
-        Es apenas normal que me dan ganas de saber<br>
-        ¿Cómo estás?<br>
-        No sé por qué pero pensé en los dos<br>
-        Yo moría de ganas de escuchar tu voz<br>
-        Se siente igual, nada cambió<br>
-        ¿Cómo estás?<br>
-        Sigues viviendo en el 506<br>
-        Donde dejamos el primer amor<br>
-        Con 16, todo cambió<br>
-        Recuerdo por qué me enamoré<br>
-        Recuerdo lo que te enamoró<br>
-        Pero contestaste y ya se me olvidó por qué se acabó<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh<br>
-        Se acabó<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
-        Oh-oh oh-oh, oh-oh oh-oh<br>
-        Se acabó
-    </p>
+    <main class="body">
+        <div class="items">
+            <div class="apartado alumnos">
+                <div class="seccion">
+                    <div class="icon"><i class="fa-solid fa-graduation-cap"></i></div>
+                    <p class="text_icon">Alumnos</p>
+                </div>
+                <div class="secciones">
+                    <a href="" class="opcion">Ver alumnos por curso</a>
+                </div>
+            </div>
+            <div class="apartado rites">
+                <div class="seccion">
+                    <div class="icon"><i class="fa-solid fa-folder-tree"></i></div>
+                    <p class="text_icon">R.I.T.E.</p>
+                </div>
+                <div class="secciones">
+                    <a href="rite.php" class="opcion">Ver R.I.T.E.</a>
+                    <span class="separacion">
+                        <div class="circulo"></div>
+                        <div class="linea"></div>
+                        <div class="circulo"></div>
+                    </span>
+                    <a href="cargar-rite.php" class="opcion">Cargar R.I.T.E.</a>
+                </div>
+            </div>
+            <div class="apartado materias">
+                <div class="seccion">
+                    <div class="icon"><i class="fa-solid fa-book"></i></div>
+                    <p class="text_icon">Materias</p>
+                </div>
+                <div class="secciones">
+                    <a href="materias.php" class="opcion">Ver materias</a>
+                </div>
+            </div>
+        </div>
+    </main>
 
 
     <script src="https://kit.fontawesome.com/d5f1649c82.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
+
+
+<p>
+    Tu número en un papel <br>
+    Recuerdos de una canción <br>
+    Tardes pretendiendo ver televisión <br>
+    Primero siente la piel <br>
+    Y luego lo siente el corazón <br>
+    Por ti iré a tu calle, me hace ver que <br>
+    Recuerdo por qué me enamoré <br>
+    Recuerdo lo que te enamoró <br>
+    Pero estoy llamando porque muero por saber<br>
+    ¿Cómo estás?<br>
+    No sé por qué pero pensé en los dos<br>
+    Moría de ganas de escuchar tu voz<br>
+    Se siente igual, nada cambió<br>
+    ¿Cómo estás?<br>
+    Sigues viviendo en el 506<br>
+    Donde dejamos el primer amor<br>
+    Con 16, todo cambió<br>
+    Recuerdo por qué me enamoré<br>
+    Recuerdo lo que te enamoró<br>
+    Pero contestaste y ya se me olvidó por qué se acabó<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh<br>
+    Se acabó<br>
+    Ayer jugamos a escondidas con otras intenciones<br>
+    Hoy te encuentro escondida en más de 15 canciones<br>
+    Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
+    Es apenas normal que me dan ganas de saber<br>
+    Si sigues yendo a Cartagena de vacaciones<br>
+    Si todavía le tienes miedo a los aviones<br>
+    Y qué le voy a hacer (qué) si no te puedo ver (no)<br>
+    Es apenas normal que me dan ganas de saber<br>
+    ¿Cómo estás?<br>
+    No sé por qué pero pensé en los dos<br>
+    Yo moría de ganas de escuchar tu voz<br>
+    Se siente igual, nada cambió<br>
+    ¿Cómo estás?<br>
+    Sigues viviendo en el 506<br>
+    Donde dejamos el primer amor<br>
+    Con 16, todo cambió<br>
+    Recuerdo por qué me enamoré<br>
+    Recuerdo lo que te enamoró<br>
+    Pero contestaste y ya se me olvidó por qué se acabó<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh<br>
+    Se acabó<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh (se acabó)<br>
+    Oh-oh oh-oh, oh-oh oh-oh<br>
+    Se acabó
+</p>
