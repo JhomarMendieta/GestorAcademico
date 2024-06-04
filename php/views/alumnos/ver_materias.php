3d8c17bfc2290df4f58d1bdcb93d3b0919da2bbe
JhomarMendieta <dkname02@gmail.com>
2024-06-04 20:18:07 -0300

    favicon agregado a todos los archivos - Jhomar

--- php/views/alumnos/ver_materias.php
+++ php/views/alumnos/ver_materias.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../../img/LogoEESTN1.png" type="image/x-icon">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="./ver_materias.css">
