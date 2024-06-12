5f819d6f8413db47dce6c5b2e38da23e7d68ece3
Tomás Arias <tomasariascontacto@gmail.com>
2024-06-12 15:37:05 -0300

    Cambios en el menu

--- php/views/profesores/menu.php
+++ php/views/profesores/menu.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="menu.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="shortcut icon" href="../../../img/LogoEESTN1.png" type="image/x-icon">
     <title>Menu</title>
@@ -10,7 +11,9 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="menu.php">Navbar</a>
+      <a id="logo" class="navbar-brand" href="menu.php">
+                <img src="../../../img/LogoEESTN1.png" alt="">
+      </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,12 +38,26 @@
     </div>
   </div>
 </nav>
+<div class="titulo-menu">
+    <h1>Menú</h1>
+  </div>
+<div class="main-container">
 
-<a href="ver_alumnos.php">Ver alumnos</a>
-<a href="actualizar_rite.php">Actualizar RITE</a>
-<a href="ver_rite.php">Ver RITE</a>
-<a href="ver_materia.php">Ver materias</a>
-<a href="gestionar_indicadores.php">Gestionar indicadores</a>
+        <div class="column">
+            <h2>ALUMNOS</h2>
+            <a href="ver_alumnos.php" class="btn btn-secondary">Ver Alumnos en curso</a>
+        </div>
+        <div class="column">
+            <h2>R.I.T.E</h2>
+            <a href="ver_rite.php" class="btn btn-secondary">Ver RITE</a>
+            <a href="actualizar_rite.php" class="btn btn-secondary">Actualizar RITE</a>
+            <a href="gestionar_indicadores.php" class="btn btn-secondary">Gestionar indicadores</a>
+        </div>
+        <div class="column">
+            <h2>MATERIAS</h2>
+            <a href="ver_materia.php" class="btn btn-secondary">Ver Materias</a>
+        </div>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
