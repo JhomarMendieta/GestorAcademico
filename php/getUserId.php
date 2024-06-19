765e8485dd30a797ce7d0660f9d54f12495ded3e
Tomás Arias <tomasariascontacto@gmail.com>
2024-06-19 20:10:40 -0300

    modularicé la navbar y agregue boton de cierre de sesión

--- php/getUserId.php
+++ php/getUserId.php
@@ -3,6 +3,7 @@
         include 'conn.php';
         if (isset($_SESSION["id"])) {
             $userId = $_SESSION["id"];
+            $userName = $_SESSION["nombre_usuario"];
 
         } else {
             header("Location: ../index.html");
