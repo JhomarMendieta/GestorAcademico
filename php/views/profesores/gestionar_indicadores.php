f6613107e3b0272aa1024a5b1211efdcd38f2769
Tomás Arias <tomasariascontacto@gmail.com>
2024-06-19 20:45:28 -0300

    moví los archivos a su ubicacion correcta

--- php/views/profesores/gestionar_indicadores.php
+++ php/views/profesores/gestionar_indicadores.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../../../img/LogoEESTN1.png" type="image/x-icon">
-    <link rel="stylesheet" href="gestionar_indicador.css">
+    <link rel="stylesheet" href="../../../css/profesores/gestionar_indicador.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Gestionar indicadores</title>
 </head>
@@ -199,7 +199,7 @@
         </div>
     </div>
 
-    <script src="gestionar_indicador.js"></script>
+    <script src="../../../js/profesores/gestionar_indicador"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
