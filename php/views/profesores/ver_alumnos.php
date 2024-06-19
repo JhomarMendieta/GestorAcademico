cbc65607d33561b6aeac04edebc513d4b2f82fda
Tomás Arias <tomasariascontacto@gmail.com>
2024-06-19 20:23:34 -0300

    corregí un pequeño error

--- php/views/profesores/ver_alumnos.php
+++ php/views/profesores/ver_alumnos.php
@@ -11,40 +11,13 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a id="logo" class="navbar-brand" href="menu.php">
-                <img src="../../../img/LogoEESTN1.png" alt="">
-            </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="ver_alumnos.php">Ver alumnos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="actualizar_rite.php">Actualizar RITE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="ver_rite.php">Ver RITE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="ver_materia.php">Ver materias</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="gestionar_indicadores.php">Gestionar indicadores</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div class="container-alumnos">
+<?php
+include '../../conn.php';
+include './navbar_profesores.php';
+?>  
+<div class="container-alumnos">
   <?php
 // Incluir el archivo de conexión a la base de datos 
-include '../../conn.php';
-include '../../getUserId.php';
 
 // Obtener el id del profesor basado en el id del usuario
 $query = "SELECT numLegajo FROM profesores WHERE id_usuario = ?";
@@ -153,3 +126,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['materia_id'])) {
 // Cerrar la conexión a la base de datos
 $conn->close();
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
