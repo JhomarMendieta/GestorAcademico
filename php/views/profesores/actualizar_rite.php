56e92e3bc3f6243219b137e92304dffca04ea225
totits <109104271+totits@users.noreply.github.com>
2024-06-19 21:03:57 -0300

    rehice la ruta del js

--- php/views/profesores/actualizar_rite.php
+++ php/views/profesores/actualizar_rite.php
@@ -264,7 +264,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['instancia']) && isset(
 
         ?>
     </div>
-    <script src="../../../js/profesores/actualizar_rite"></script>
+    <script src="../../../js/profesores/actualizar_rite.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
