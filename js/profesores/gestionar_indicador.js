509164f51eb09c073b4f51dd12696d6c17346aad
Tomás Arias <tomasariascontacto@gmail.com>
2024-06-21 18:43:54 -0300

    pequeño cambio en la edicion de indicadores

--- js/profesores/gestionar_indicador.js
+++ js/profesores/gestionar_indicador.js
@@ -1,11 +1,17 @@
-function showForm(formId) {
-    var forms = document.getElementsByClassName('form-container');
-    for (var i = 0; i < forms.length; i++) {
-        forms[i].style.display = 'none';
-    }
-    document.getElementById(formId).style.display = 'table-row';
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var editModal = document.getElementById('editModal');
+    editModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var id = button.getAttribute('data-id');
+        var nombre = button.getAttribute('data-nombre');
+        var instancia = button.getAttribute('data-instancia');
 
-function hideForm(formId) {
-    document.getElementById(formId).style.display = 'none';
-}
+        var modalIdInput = editModal.querySelector('#editNotaId');
+        var modalNombreInput = editModal.querySelector('#editNombre');
+        var modalInstanciaSelect = editModal.querySelector('#editInstancia');
+
+        modalIdInput.value = id;
+        modalNombreInput.value = nombre;
+        modalInstanciaSelect.value = instancia;
+    });
+});
