5efca234574684e3b387319fea80568f28527d40
carlos soliz <carlos.soliz.t1vl@gmail.com>
2024-06-24 10:54:51 -0300

    final

--- php/login.php
+++ php/login.php
@@ -10,9 +10,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Consulta para verificar las credenciales del usuario
-    $sql = "SELECT id, nombre_usuario, mail, rol, contrasenia FROM usuario WHERE nombre_usuario = '$username'";
-    $result = $conn->query($sql);
+    // Consulta con sentencia preparada
+    $sql = "SELECT id, nombre_usuario, mail, rol, contrasenia FROM usuario WHERE nombre_usuario = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username); // "s" indica que es un valor de tipo string
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
@@ -25,10 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION["rol"] = $row["rol"];
             $_SESSION["logged_in"] = true;
             header("Location: ../index.php");
+            exit; // Importante: salir del script después de redirigir
         } else {
-            echo "Nombre de usuario o contraseña incorrectos";
+            echo "Credenciales inválidas"; // Mensaje genérico
         }
+    } else {
+        echo "Credenciales inválidas"; // Mensaje genérico
     }
 
+    $stmt->close();
     $conn->close();
 }
